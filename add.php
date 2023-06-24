74dae08488c17011e5ef55880368995b5c0477b0
julicode97 <jlondono433@gmail.com>
2023-06-24 12:30:19 -0500

    validation & bind parameters

--- add.php
+++ add.php
@@ -1,11 +1,21 @@
 <?php
   require "database.php";
+  $error = null;
+
   if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
-    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) values('$name','$phoneNumber')");
-    $statement->execute();
-    header("Location: index.php");
+    if(empty($_POST["name"]) || empty($_POST["phone_number"])){
+      $error = "Please fill all the fields";
+    }else if(strlen($_POST["phone_number"]) < 9){
+      $error = "Phone number must be at least 9 characters";
+    }else{
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) values(:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
+      header("Location: index.php");
+    }
   }
 ?>
 <!DOCTYPE html>
@@ -69,17 +79,22 @@
             <div class="card">
               <div class="card-header">Add new Contact</div>
               <div class="card-body">
+                <?php if($error != null): ?>
+                  <p class="text-danger">
+                    <?= $error ?>
+                  </p>
+                <?php endif ?>
                 <form method="POST" action="add.php">
                   <div class="form-group row mt-2">
                     <label for="name" class="col-md-4 col-form-label text-md-rigth">Name</label>
                     <div class="col-md-6">
-                      <input id="name" type="text" class="form-control" name="name" required autocomplete="name">
+                      <input id="name" type="text" class="form-control" name="name" autocomplete="name">
                     </div>
                   </div>
                   <div class="form-group row mt-2">
                     <label for="phone_number" class="col-md-4 col-form-label text-md-rigth">Phone Number</label>
                     <div class="col-md-6">
-                      <input id="phone_number" type="text" class="form-control" name="phone_number" required autocomplete="phone_number">
+                      <input id="phone_number" type="text" class="form-control" name="phone_number" autocomplete="phone_number">
                     </div>
                   </div>
                   <div class="form-group row mt-2">
